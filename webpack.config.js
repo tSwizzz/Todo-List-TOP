8fdfaddc43250ce90119faf32c4999cc0f8d6249
Eddy G <eddy.spambari@gmail.com>
2022-11-25 04:15:35 -0800

    implmented loaders for css, images, and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 
 module.exports = {
+mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -12,6 +13,14 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   }

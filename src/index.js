2bdfb060e79a60f59acafacf10fafaa677ede344
Eddy G <eddy.spambari@gmail.com>
2022-11-25 04:12:58 -0800

    generated images and header info for the header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+import todoIcon from './images/todo-logo.png';
+
+//generates header logo and title and appends them
+const headerContainer = document.getElementById("headerContainer");
+
+const todoLogo = new Image();
+todoLogo.src = todoIcon;
+todoLogo.classList.add("todoLogo");
+
+const headerTitle = document.createElement('h1');
+headerTitle.classList.add("headerTitle");
+headerTitle.textContent = "To-do List";
+
+headerContainer.append(todoLogo, headerTitle);
+
+//
+const projectList = document.querySelector("projectList");
+
+

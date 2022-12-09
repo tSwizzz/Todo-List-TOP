d950b2f51d3c61b12b79bfe8400df018b20145b9
Eddy G <eddy.spambari@gmail.com>
2022-12-08 18:22:39 -0800

    added title chager

---
+++ src/modules/projectDisplay.js
@@ -0,0 +1,11 @@
+const projectDisplayHeader = document.querySelector(".projectDisplayHeader");
+
+const projectDisplayTitle = document.createElement('h1');
+projectDisplayTitle.classList.add("projectDisplayTitle");
+projectDisplayTitle.textContent = "Your projects display here!";
+projectDisplayHeader.appendChild(projectDisplayTitle);
+
+
+
+
+
